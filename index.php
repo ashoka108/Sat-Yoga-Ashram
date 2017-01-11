b8a4e858d5a7d30a92e749ba8470c07e1b7e0112
Xavier Fernandez <xavierf@Xaviers-MacBook-Pro.local>
2017-01-11 11:33:49 -0600

    Added files for a basic joomla template using semantic UI

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+  <head>
+    <jdoc:include type="head" />
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-3.1.1.min.js"></script>
+    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/semantic/semantic.min.js"></script>
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/semantic/semantic.min.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+  </head>
+  <body>
+  <jdoc:include type="modules" name="top" />
+  <jdoc:include type="component" />
+  <jdoc:include type="modules" name="bottom" />
+<div class="ui grid">
+  <div class="column" style="text-align: center; background-color: gray;">1</div>
+  <div class="column" style="text-align: center; background-color: lightGray;">2</div>
+  <div class="column" style="text-align: center; background-color: gray;">3</div>
+  <div class="column" style="text-align: center; background-color: lightGray;">4</div>
+  <div class="column" style="text-align: center; background-color: gray;">5</div>
+  <div class="column" style="text-align: center; background-color: lightGray;">6</div>
+  <div class="column" style="text-align: center; background-color: gray;">7</div>
+  <div class="column" style="text-align: center; background-color: lightGray;">8</div>
+  <div class="column" style="text-align: center; background-color: gray;">9</div>
+  <div class="column" style="text-align: center; background-color: lightGray;">10</div>
+  <div class="column" style="text-align: center; background-color: gray;">11</div>
+  <div class="column" style="text-align: center; background-color: lightGray;">12</div>
+</div>
+  </body>
+</html>
