5d7af064d2cb531cefd7d74506fc984c4cdf04fd
Xavier Fernandez <xavier@crservers.com>
2017-01-20 20:09:23 -0600

    Template cleanup

--- index.php
+++ index.php
@@ -16,10 +16,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <!-- Include Joomla Header code -->
     <jdoc:include type="head" />
-    <!--  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" /> -->
-    <!-- Load Semantic UI Library -->
-<!--     <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/jquery-3.1.1.min.js"></script> -->
     <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/semantic/semantic.min.js"></script>
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/semantic/semantic.min.css" type="text/css" />
     <!-- Load Custom CSS & JS-->
@@ -35,47 +31,102 @@
       <?php endif; ?>
     </div>
     <div class="pusher">
-    <!-- Banner segment -->
-      <div class="ui inverted vertical masthead segment">
-        <div class="ui responsive grid">
-          <!-- Top header menu -->
-          <div class="ui tablet computer only eight wide column">
-            <?php if($this->countModules('top')) : ?>
-            <jdoc:include type="modules" name="top" style="none" />
-            <?php endif; ?>
-          </div>
-          <!-- Search -->
-          <div class="ui tablet computer only four wide column">
-            <?php if($this->countModules('search')) : ?>
-              <jdoc:include type="modules" name="search" style="none" />
+
+    <!-- Top Menu -->
+    <header>
+    <?php if($this->countModules('top-left') || $this->countModules('top-right') || $this->countModules('search')) : ?>
+      <div class="ui left aligned grid">
+        <div class="left floated left aligned six wide column">
+          <jdoc:include type="modules" name="top-left" style="none" />
+        </div>
+        <div class="right floated right aligned three wide column">
+          <jdoc:include type="modules" name="top-right" style="none" />
+        </div>
+        <div class="right floated right aligned three wide column">
+          <jdoc:include type="modules" name="search" style="none" />
+        </div>
+    <?php endif; ?>
+
+    <!-- Main menu -->
+      <div class="main-menu navbar">
+        <div class="ui responsive grid container">
+          <div class="computer only centered twelve wide column">
+            <?php if($this->countModules('main-menu')) : ?>
+              <jdoc:include type="modules" name="main-menu" style="none" />
             <?php endif; ?>
           </div>
            <!-- Mobile menu -->
-          <div class="ui mobile tablet only column" onclick="toggleMobileMenu()">
+          <div class="mobile tablet only two wide left floated column" onclick="toggleMobileMenu()">
             <i class="big sidebar icon"></i>
             <?php echo JText::_( 'Menu' );?>
           </div>
         </div>
-           <!-- Main menu -->
-        <div class="ui computer only centered responsive container">
-          <?php if($this->countModules('main-menu')) : ?>
-            <jdoc:include type="modules" name="main-menu" style="none" />
-          <?php endif; ?>
-        </div>
-        <jdoc:include type="modules" name="image-slider" style="none" />
       </div>
+    </header>
+
+    <!-- Mobile menu -->
+    <jdoc:include type="modules" name="image-slider" style="none" />
+
       <div class="ui container">
-        <jdoc:include type="modules" name="breadcrumbs" style="none" />
-      </div>
-      <div class="ui raised segment container">
-        <div class="messages">
+        <?php if($this->countModules('breadcrumbs')) : ?>
+          <jdoc:include type="modules" name="breadcrumbs" style="none" />
+        <?php endif; ?>
+        <?php if($this->countModules('messages')) : ?>
           <jdoc:include type="modules" name="messages" style="none" />
           <jdoc:include type="message" />
+        <?php endif; ?>
+      </div>
+
+    <?php if ($this->countModules('right-sidebar'))
+     {
+       $componentwidth = "twelve wide";
+     }
+     else
+     {
+       $componentwidth = "sixteen wide";
+     }
+     ?>
+      <div class="ui raised segment container">
+        <?php if($this->countModules('above-component')) : ?>
+          <jdoc:include type="modules" name="above-component" style="none" />
+        <?php endif; ?>
+        <div class="ui grid container">
+          <div class="<?php echo $componentwidth ?> column">
+            <jdoc:include type="component" />
+          </div>
+          <?php if($this->countModules('right-sidebar')) : ?>
+              <div class="four wide column" id="sidebarContext">
+                <div class="ui sticky">
+                <jdoc:include type="modules" name="right-sidebar" style="none" />
+                </div>
+              </div>
+          <?php endif; ?>
         </div>
-        <jdoc:include type="modules" name="above-component" style="none" />
-        <jdoc:include type="component" />
-        <jdoc:include type="modules" name="right-sidebar" style="none" />
-        <jdoc:include type="modules" name="below-component" style="none" />
+          <?php if($this->countModules('below-component')) : ?>
+            <jdoc:include type="modules" name="below-component" style="none" />
+          <?php endif; ?>
+      </div>
+      <footer class="ui six column stackable grid container">
+        <div class="column">
+          <jdoc:include type="modules" name="footer-1" style="xhtml" />
+        </div>
+        <div class="column">
+          <jdoc:include type="modules" name="footer-2" style="xhtml" />
+        </div>
+        <div class="column">
+          <jdoc:include type="modules" name="footer-3" style="xhtml" />
+        </div>
+        <div class="column">
+          <jdoc:include type="modules" name="footer-4" style="xhtml" />
+        </div>
+        <div class="two wide column">
+          <jdoc:include type="modules" name="footer-5" style="xhtml" />
+        </div>
+      </footer>
+      <div class="ui container">
+        <?php if($this->countModules('copyright')) : ?>
+          <jdoc:include type="modules" name="copyright" style="none" />
+        <?php endif; ?>
       </div>
     </div> <!-- Ends Pusher -->
    <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/template.js"></script>
